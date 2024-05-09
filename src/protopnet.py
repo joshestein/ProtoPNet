ef5f6b8a09a6d054c7c4177c533954cf78901eb7
Josh <josh.e.stein@gmail.com>
2024-05-09 17:39:51 -0400

    Rename `conv_net` -> `pretrained_conv_net`

--- src/protopnet.py
+++ src/protopnet.py
@@ -25,11 +25,11 @@ class ProtoPNet(nn.Module):
         """
         super().__init__()
 
-        self.conv_net = torch.hub.load("pytorch/vision:v0.10.0", base_model, pretrained=True)
-        del self.conv_net.classifier  # Remove classification layers
+        self.pretrained_conv_net = torch.hub.load("pytorch/vision:v0.10.0", base_model, pretrained=True)
+        del self.pretrained_conv_net.classifier  # Remove classification layers
 
         # TODO: support other base models, conditionally construct `expected input_shape`
-        conv_output_channels = get_model_output_shape(self.conv_net, (1, 3, 224, 224))[1]
+        conv_output_channels = get_model_output_shape(self.pretrained_conv_net, (1, 3, 224, 224))[1]
 
         self.additional_layers = nn.Sequential(
             nn.Conv2d(conv_output_channels, output_channels, 1),  # First 1x1 convolution
@@ -43,8 +43,6 @@ class ProtoPNet(nn.Module):
         self.fully_connected = nn.Linear(prototypes_per_class, num_output_classes, bias=False)
 
     def forward(self, x):
-        """Images should be 224 x 224 x 3"""
-        # Pre-trained conv net
-        x = self.conv_net(x)
+        x = self.pretrained_conv_net(x)
         x = self.additional_layers(x)
         return x
