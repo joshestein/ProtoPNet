cf8454aca752ef932dd868ec8b5d73f15b57c56a
Josh <josh.e.stein@gmail.com>
2024-05-24 17:20:30 -0400

    Prototype layers and activation

--- src/protopnet.py
+++ src/protopnet.py
@@ -1,4 +1,5 @@
 import torch
+import torch.nn.functional as F
 from torch import nn
 
 
@@ -27,15 +28,40 @@ class ProtoPNet(nn.Module):
             nn.Sigmoid(),
         )
 
-        num_prototypes = prototypes_per_class * num_output_classes
+        self.num_prototypes = prototypes_per_class * num_output_classes
+        prototype_shape = (self.num_prototypes, output_channels, 1, 1)
+
         # Each class has a onehot prototype representation
-        self.prototype_onehot_class_representation = torch.zeros((num_output_classes, num_prototypes))
+        self.prototype_onehot_class_representation = torch.zeros((num_output_classes, self.num_prototypes))
 
         # TODO: Xavier initialisation
-        self.prototypes = nn.Parameter(torch.randn(num_prototypes, output_channels, 1, 1))
-        self.fully_connected = nn.Linear(num_prototypes, num_output_classes, bias=False)
+        self.prototypes = nn.Parameter(torch.randn(prototype_shape))
+        self.ones = nn.Parameter(torch.ones(prototype_shape), requires_grad=False)
+        self.fully_connected = nn.Linear(self.num_prototypes, num_output_classes, bias=False)
 
     def forward(self, x):
         x = self.pretrained_conv_net(x)
         x = self.additional_layers(x)
-        return x
+
+        # Do the prototype stuff! This is basically copied from the original repo, I don't really understand it :(
+        distances = self.prototype_distances(x)
+        min_distances = -F.max_pool2d(-distances, kernel_size=(distances.size()[2], distances.size()[3]))
+        min_distances = min_distances.view(-1, self.num_prototypes)
+        x = self.prototype_activations(distances)
+
+        x = self.fully_connected(x)
+        return x, min_distances
+
+    def prototype_distances(self, x):
+        x_convolved_prototypes = torch.sum(F.conv2d(x, weight=self.prototypes))
+        prototypes_squared = torch.sum(self.prototypes**2, dim=(1, 2, 3))
+        intermediate = -2 * x_convolved_prototypes + prototypes_squared.view(-1, 1, 1)
+
+        x2_convolved_patches = F.conv2d(x**2, weight=self.ones)
+
+        distances = F.relu(x2_convolved_patches + intermediate)
+        return distances
+
+    def prototype_activations(self, distances, epsilon=0.001):
+        distances = distances.view(-1, self.prototypes.shape[0])
+        return torch.log((distances + 1) / (distances + epsilon))
