90554ee1e606128c5dd6e83f04bad9a95f33eb74
Josh <josh.e.stein@gmail.com>
2024-06-02 16:12:45 -0400

    Add convex start epoch

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+CONVEX_OPTIMISATION_START_EPOCH = 10
 CONVEX_OPTIMISATION_STEPS = 20
 NUM_TRAINING_EPOCHS = 1000
 NUM_WARM_EPOCHS = 5  # How many epochs to only train additional (not pre-trained) layers
